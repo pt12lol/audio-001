62c4713c310a934236f9fcbb0faa6e89845d0ed8
Piotr Tokarski <piotr.tokarski@partners.mbank.pl>
2015-01-27 18:41:46 +0100

    refactor and spectrogram in progress

--- audio-001/Notes.py
+++ audio-001/Notes.py
@@ -4,18 +4,22 @@ import matplotlib.pyplot as plt
 import scipy.io.wavfile as wav
 import scipy.signal as sgnl
 import scipy.fftpack as fp
+import scipy.signal.windows as windows
 
 
 
-def get_fragment(x, sec_begin = 0, sec_end = -1, fs = 44100):
-    begin = get_samples(sec_begin, fs)
-    end = get_samples(sec_end if sec_end == -1 else len(x) / float(fs), fs)
+# samples management ###########################################################
+def get_samples(x, sec_begin = 0, sec_end = -1, fs = 44100):
+    begin = get_samplenum(sec_begin, fs)
+    end = get_samplenum(sec_end if sec_end != -1 else len(x) / float(fs), fs)
     return x[begin:end]
 
-def get_samples(sec, fs = 44100):
-    return int(fs * sec)
+def get_samplenum(sec, fs = 44100):
+    return fs * sec
+################################################################################
 
 
+# files manipulation ###########################################################
 def write_wav(filepath, x, fs = 44100):
     wav.write(
         filepath, fs,
@@ -30,12 +34,15 @@ def write_sine(filepath, A = 20000, f = 440, fs = 44100, t = 1):
 
 def read_wav(filepath, sec_begin = 0, sec_end = -1):
     fs, x = wav.read(filepath)
-    return fs, get_fragment(x, sec_begin, sec_end, fs)
+    return fs, get_samples(x, sec_begin, sec_end, fs)
 
 def read_wavnormalized(filepath, sec_begin = 0, sec_end = -1):
     fs, x = wav.read(filepath)
-    return fs, get_fragment(x / float(np.iinfo(np.int16).max), sec_begin, sec_end, fs)
+    return fs, get_samples(x / float(np.iinfo(np.int16).max), sec_begin, sec_end, fs)
+################################################################################
 
+
+# plot drawing #################################################################
 def show_plot(title, x, y):
     y_min = float(min(y))
     y_max = float(max(y))
@@ -48,9 +55,10 @@ def show_plot(title, x, y):
 def show_wavplot(filepath, read_func = read_wav, sec_begin = 0, sec_end = -1):
     fs, x = read_func(filepath, sec_begin, sec_end)
     show_plot(filepath, np.arange(0, sec_end, 1./fs), x)
+################################################################################
 
 
-
+# signal generators ############################################################
 def genRealSine(A = 1, f = 440, phi = 0, fs = 44100, t = 1):
     return A * np.cos(2 * np.pi * f * np.arange(0, t, 1.0 / fs) + phi)
 
@@ -60,15 +68,19 @@ def genComplexSine(A = 1, k = 440, N = 44100):
 
 def genWhiteNoise(A_max = 20000, fs = 44100, t = 1):
     return np.array([random.randint(-A_max, A_max) for i in range(fs * t)])
+################################################################################
 
 
+# windows helpers ##############################################################
 def rectangularWindow(M):
     return np.ones(M)
 
 def genKaiser(beta):
-    return lambda M: np.kaiser(M, beta)
+    return lambda M: windows.kaiser(M, beta)
+################################################################################
 
 
+# fourier transform helpers ####################################################
 def __fft_acc(x, cutoff):
     N = len(x)
     X_even = fft(x[::2])
@@ -80,19 +92,6 @@ def __fft_acc(x, cutoff):
             X_even + factor[N / 2:] * X_odd
         ]
     )
-
-def __ifft_acc(X, cutoff):
-    N = len(X)
-    x_even = ifft(X[::2])
-    x_odd = ifft(X[1::2])
-    factor = np.exp(2j * np.pi * np.arange(N) / N)
-    return np.concatenate(
-        [
-            x_even + factor[:N / 2] * x_odd,
-            x_even + factor[N / 2:] * x_odd
-        ]
-    ) / 2
-
 
 def dft(x):
     N = len(x)
@@ -107,9 +106,11 @@ def fft(x, cutoff = 32):
 
 def genFft(cutoff):
     return lambda x: fft(x, cutoff)
+################################################################################
 
-def genBufferedDft(x, w = rectangularWindow, dft_len = -1, dft_func = fp.fft):
-    N = dft_len if dft_len != -1 else len(x)
+
+# dft analyzis helpers #########################################################
+def genBufferedDft(x, w, N, dft_func = fp.fft):
     x_len = len(x)
     hM1 = int(math.floor((x_len + 1) / 2))
     hM2 = int(math.floor(x_len / 2))
@@ -121,23 +122,73 @@ def genBufferedDft(x, w = rectangularWindow, dft_len = -1, dft_func = fp.fft):
     )
     return dft_func(dft_buffer)
 
-def genMagnitudeSpectrum(x, w = rectangularWindow, dft_len = -1, dft_func = fp.fft):
-    hN = dft_len / 2
-    X = genBufferedDft(x, w, dft_len, dft_func)
+def genMagnitudeSpectrum_dft(x, w, N, dft_func = fp.fft):
+    hN = N / 2
+    X = genBufferedDft(x, w, N, dft_func)
     return 20 * np.log10(np.real(abs(X[:hN])))
 
-def genPhaseSpectrum(x, w = rectangularWindow, dft_len = -1, dft_func = fp.fft):
-    hN = dft_len / 2
-    X = genBufferedDft(x, w, dft_len, dft_func)
+def genPhaseSpectrum_dft(x, w, N, dft_func = fp.fft):
+    hN = N / 2
+    X = genBufferedDft(x, w, N, dft_func)
     return np.unwrap(np.angle(X[:hN]))
 
-def genSpectrums(x, w = rectangularWindow, dft_len = -1, dft_func = fp.fft):
-    hN = dft_len / 2
-    X = genBufferedDft(x, w, dft_len, dft_func)
+def genSpectrums_dft(x, w, N, dft_func = fp.fft):
+    hN = N / 2
+    X = genBufferedDft(x, w, N, dft_func)
     mX = 20 * np.log10(np.real(abs(X[:hN])))
     pX = np.unwrap(np.angle(X[:hN]))
     return mX, pX
+################################################################################
 
+
+# stft analyzis helpers ########################################################
+def genSamplesOfStft(x, w, M, H, dft_func = fp.fft):    
+    hM1 = int(math.floor((M + 1) / 2))
+    hM2 = int(math.floor(M / 2))
+    samples_num = (len(x) - 2 * hM1) / H
+    lv = np.arange(hM1, hM1 * samples_num, H)
+    return np.array([x[l - hM1:l + hM2] for l in lv])
+
+def genMagnitudeSpectrum_stft(x, w, M, w_len, H, dft_func = fp.fft):
+    xlv = genSamplesOfStft(x, w, M, H, dft_func)
+    return np.transpose(
+        np.array(
+            [genMagnitudeSpectrum_dft(xl, w, w_len, dft_func) for xl in xlv]
+        )
+    )
+
+def genPhaseSpectrum_stft(x, w, M, w_len, H, dft_func = fp.fft):
+    xlv = genSamplesOfStft(x, w, M, H, dft_func)
+    return np.transpose(
+        np.array(
+            [genPhaseSpectrum_dft(xl, w, w_len, dft_func) for xl in xlv]
+        )
+    )
+
+def genSpectrums_stft(x, w, M, w_len, H, dft_func = fp.fft):
+    xlv = genSamplesOfStft(x, w, M, H, dft_func)
+    return zip(
+        *np.transpose(
+            np.array(
+                [genSpectrums_dft(xl, w, w_len, dft_func) for xl in xlv]
+            )
+        )
+    )
+################################################################################
+
+
+# inverse fourier transform helpers ############################################
+def __ifft_acc(X, cutoff):
+    N = len(X)
+    x_even = ifft(X[::2])
+    x_odd = ifft(X[1::2])
+    factor = np.exp(2j * np.pi * np.arange(N) / N)
+    return np.concatenate(
+        [
+            x_even + factor[:N / 2] * x_odd,
+            x_even + factor[N / 2:] * x_odd
+        ]
+    ) / 2
 
 def idft(X):
     N = len(X)
@@ -152,11 +203,13 @@ def ifft(X, cutoff = 32):
 
 def genIfft(cutoff):
     return lambda X: ifft(X, cutoff)
+################################################################################
 
-def genSignal(mX, pX, x_len = -1, w = rectangularWindow, idft_func = fp.ifft):
+
+# dft synthesis helpers ########################################################
+def genSignal(mX, pX, w, M, idft_func = fp.ifft):
     N = len(mX) * 2
     hN = N / 2
-    M = x_len if x_len != -1 else N
     hM1 = int(math.floor((M + 1) / 2))
     hM2 = int(math.floor(M / 2))
     X = np.concatenate(
@@ -173,76 +226,87 @@ def genSignal(mX, pX, x_len = -1, w = rectangularWindow, idft_func = fp.ifft):
     w_x = np.array([item if item != 0 else np.finfo(float).eps for item in w(M)])
     w_sum = sum(w_x)
     return x * w_sum / w_x
+################################################################################
 
 
 
-def analize_sinusoid(
-    real = True, amp = 1, freq = 110, hop_size = 10,
-    dft_rate = 1, dft_func = fp.fft, idft_func = fp.ifft,
-    window_func = rectangularWindow
+def dft_analizeSinusoid(
+    real = True, A = 1, f = 110, H = 10,
+    N = 50000, dft_func = fp.fft, idft_func = fp.ifft,
+    w = rectangularWindow
 ):
 
     x = (
-        genRealSine(A = amp, f = freq) if real else
-        genComplexSine(A = amp, k = freq)
-    )[::hop_size]
+        genRealSine(A = A, f = f) if real else
+        genComplexSine(A = A, k = f)
+    )[::H]
 
     fs = 44100.0
-    N = len(x)
-    print('len(x) == %s' % N)
+    x_len = len(x)
 
     X = abs(dft_func(x))
-    mX, pX = genSpectrums(x, w = window_func, dft_len = dft_rate * N, dft_func = dft_func)
-    y = genSignal(mX = mX, pX = pX, x_len = N, w = window_func, idft_func = idft_func)
+    mX, pX = genSpectrums_dft(x, w, N, dft_func = dft_func)
+    y = genSignal(mX, pX, w, x_len, idft_func = idft_func)
     Y = abs(dft_func(y))
 
-    show_plot('Wave x', np.arange(N), np.real(x))
-    show_plot('DFT of wave x', np.arange(N), np.real(X))
-    show_plot('Magnitude Spectrum of x', np.arange(0, N / 2.0, 1.0 / dft_rate), mX)
-    show_plot('Phase Spectrum of x', np.arange(0, N / 2.0, 1.0 / dft_rate), pX)
-    show_plot('Wave y', np.arange(N), np.real(y))
-    show_plot('DFT of wave y', np.arange(N), np.real(Y))
-    
-    mX_max = max(mX)
-    print('max magnitude spectrum of wave x: %s' % [(idx / dft_rate, val) for idx, val in enumerate(mX) if val == mX_max])
+    show_plot('Wave x', np.arange(x_len), np.real(x))
+    show_plot('DFT of wave x', np.arange(x_len), np.real(X))
+    show_plot('Magnitude Spectrum of x', np.arange(0, fs / (2.0 * H), fs / (H * N)), mX)
+    show_plot('Phase Spectrum of x', np.arange(0, fs / (2.0 * H), fs / (H * N)), pX)
+    show_plot('Wave y', np.arange(x_len), np.real(y))
+    show_plot('DFT of wave y', np.arange(x_len), np.real(Y))
 
 
-def analyze_wav(input_filepath, dft_rate = 1, window_func = rectangularWindow, output_filepath = 'test.wav'):
+def dft_analyzeWav(
+    input_filepath, output_filepath = 'test.wav',
+    N = 50000, w = rectangularWindow
+):
 
     fs, x = read_wavnormalized(input_filepath)
-    N = len(x)
+    x_len = len(x)
+    secs = float(x_len) / float(fs)
 
     X = abs(fp.fft(x))
-    mX, pX = genSpectrums(x, w = window_func, dft_len = dft_rate * N, dft_func = fp.fft)
-    y = genSignal(mX = mX, pX = pX, x_len = N, w = window_func, idft_func = fp.ifft)
+    mX, pX = genSpectrums_dft(x, w, N * secs, dft_func = fp.fft)
+    y = genSignal(mX, pX, w, x_len, idft_func = fp.ifft)
     Y = abs(fp.fft(y))
 
-    show_plot('Wave x', np.arange(N) / float(fs), np.real(x))
-    show_plot('DFT of wave x', np.arange(N), np.real(X))
-    show_plot('Magnitude Spectrum of x', np.arange(0, N / 2.0, 1.0 / dft_rate), mX)
-    show_plot('Phase Spectrum of x', np.arange(0, N / 2.0, 1.0 / dft_rate), pX)
-    show_plot('Wave y', np.arange(N) / float(fs), np.real(y))
-    show_plot('DFT of wave y', np.arange(N), np.real(Y))
+    show_plot('Wave x', np.arange(x_len) / float(fs), np.real(x))
+    show_plot('DFT of wave x', np.arange(x_len) / secs, np.real(X))
+    show_plot('Magnitude Spectrum of x', np.arange(0, x_len / 2, float(fs) / float(N)) / secs, mX)
+    show_plot('Phase Spectrum of x', np.arange(0, x_len / 2, float(fs) / float(N)) / secs, pX)
+    show_plot('Wave y', np.arange(x_len) / float(fs), np.real(y))
+    show_plot('DFT of wave y', np.arange(x_len) / secs, np.real(Y))
 
     write_wavnormalized(output_filepath, y, fs)
 
 
+def stft_analyzeWav(
+    input_filepath, output_filepath = 'test.wav',
+    x_len = 801, window_len = 1024, hop_len = 400, window_func = rectangularWindow
+):
+    fs, x = read_wavnormalized(input_filepath)
+    xmX = genMagnitudeSpectrum_stft(x, window_func, x_len, window_len, hop_len)
+    plt.pcolormesh(xmX)
+    plt.show()
+
+
 def dft_test(
-    real = True, amp = 1, freq = 110, hop_size = 15,
-    dft_rate = 1, dft_func = fp.fft, idft_func = fp.ifft,
-    window_func = rectangularWindow
+    real = True, A = 1, f = 110, H = 10,
+    N = 50000, dft_func = fp.fft, idft_func = fp.ifft,
+    w = rectangularWindow
 ):
 
     x = (
-        genRealSine(A = amp, f = freq) if real else
-        genComplexSine(A = amp, k = freq)
-    )[::hop_size]
+        genRealSine(A = A, f = f) if real else
+        genComplexSine(A = A, k = f)
+    )[::H]
     
-    N = len(x)
+    x_len = len(x)
 
     X = abs(dft_func(x))
-    mX, pX = genSpectrums(x, w = window_func, dft_len = dft_rate * N, dft_func = dft_func)
-    y = genSignal(mX = mX, pX = pX, x_len = N, w = window_func, idft_func = idft_func)
+    mX, pX = genSpectrums_dft(x, w, N, dft_func = dft_func)
+    y = genSignal(mX, pX, w, x_len, idft_func = idft_func)
     Y = abs(dft_func(y))
 
 
